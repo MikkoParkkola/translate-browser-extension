205993e90cc8951fcde53cec35c6f43df10c17a7
Mikko <mikko@MacBook-Pro-2.local>
2025-08-10 00:51:32 +0200

    chore(test): ignore tools/ in Jest to avoid haste-map collision; bump version to 1.4.0

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
+  modulePathIgnorePatterns: ['<rootDir>/tools/'],
+  watchPathIgnorePatterns: ['<rootDir>/tools/'],
 };
